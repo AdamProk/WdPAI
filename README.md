7efb093c0a720534f9052d2c999d256378af811c
AdamProk <adam.prokop505@gmail.com>
2023-12-21 18:36:21 +0100

    added contents of tutorials 5 to 7

---
+++ README.md
@@ -0,0 +1,13 @@
+# WdPAI Project
+
+##Opis
+
+##Wykorzystane technologie
+
+##Schemat bezy danych
+
+##Screanshoty - w przyszłości
+
+##Twórcy
+
+-Adam Prokop
---
+++ src/controllers/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'AppController.php';
+require_once __DIR__.'/../models/User.php';
+
+class SecurityContrller extends AppController{
+    public function login()
+    {
+        $user = new User('johnsnow@pk.edu.pl', 'johnsnow123', 'John', 'Snow'); 
+
+        if(!$this->isPost()){
+            return $this->render('login');
+        }
+
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        
+        if($user -> getEmail() !== $email){
+            return $this->render( 'login', ['messages' => ['User with this email does not exist']]);
+        }
+
+        if($user -> getPassword !== $password){
+            return $this->render( 'login',['messages' => ['Wrong password']]);
+        } 
+
+        //return $this->render('projects')
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("locations ($url)/projects");
+    }
+
+}
